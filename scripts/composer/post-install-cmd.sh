c22c53eda251a0f331225eeebd48af924cdda38e
bjorn <bjorn@swis.nl>
2020-11-05 14:15:48 +0100

    Update composer.json so the symlinks/sync script is run after install. Also change the installer paths to reflect the new structure.

---
+++ scripts/composer/post-install-cmd.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+main() {
+  echo "Running post install commands..."
+
+  bash scripts/sh/check-symlinks.sh
+  bash scripts/sh/publish-assets.sh
+
+  echo "Done"
+}
+
+(
+  set -euo pipefail
+  main
+)
